9bb391d31289ecf8b05b4502d0d2ff0abc6893d2
kgytis <kaziukon.gytis@gmail.com>
2022-11-29 10:27:29 +0200

    Custom Modal

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,30 @@
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+const BackDrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <BackDrop onClick={props.onClose} />,
+        document.getElementById("overlay")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        document.getElementById("overlay")
+      )}
+    </>
+  );
+};
+
+export default Modal;
