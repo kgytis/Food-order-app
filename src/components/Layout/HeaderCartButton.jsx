6d2f38671d5d1a16ecae1a1c48dc0b77654a153f
kgytis <kaziukon.gytis@gmail.com>
2022-11-29 12:20:52 +0200

    Bump effect added

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -2,16 +2,32 @@
 import classes from "./HeaderCartButton.module.css";
 // components
 import CartIcon from "../Cart/CartIcon";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnAnim, setBtnAnim] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numberOfCartitems = cartCtx.items.reduce((curr, item) => {
+  const { items } = cartCtx;
+  const numberOfCartitems = items.reduce((curr, item) => {
     return curr + item.quantity;
   }, 0);
+
+  const btnClasses = `${classes.button} ${btnAnim ? classes.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnAnim(true);
+    const timer = setTimeout(() => {
+      setBtnAnim(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button onClick={props.onShowCart} className={classes.button}>
+    <button onClick={props.onShowCart} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
